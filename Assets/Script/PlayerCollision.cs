860e011eeab7186e062e13cdfe22e1afb1ddcbac
Alonso Fritz <alonsofritz.dev@gmail.com>
2023-08-06 13:47:40 -0300

    feat: add obstacle group and stop movement and controls

---
+++ Assets/Script/PlayerCollision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+    
+    // This is a variable to reference a PlayerMovement script, to turn off when collides
+    public PlayerMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo) {
+        
+        if (collisionInfo.collider.tag == "Obstacle") {
+            movement.enabled = false;
+        }
+    }
+}
